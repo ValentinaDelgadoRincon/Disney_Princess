b6439e5c07a579ea2efb54e4ffb9864fedbca546
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-09-16 09:56:06 -0500

    feat: :sparkles: simular interfaz
    simular interfaz
    
    By @Valentina

--- curandera.js
+++ curandera.js
@@ -1,4 +1,4 @@
-import Personaje from "./Personaje.js";
+const Personaje = require('./Personaje');
 
 class Curandera extends Personaje {
     constructor(nivel = 1, inventario = []) {
@@ -7,8 +7,13 @@ class Curandera extends Personaje {
     }
 
     sanarAliado(aliado) {
-        aliado.curarse(this.poderSanacion);
-        console.log(`${this.nombre} sana a ${aliado.nombre} con ${this.poderSanacion} puntos de vida.`);
+        if (typeof aliado.curarse === "function") {
+            aliado.curarse(this.poderSanacion);
+            console.log(`${this.nombre} sana a ${aliado.nombre} con ${this.poderSanacion} puntos de vida.`);
+        } else {
+            console.log(`${aliado.nombre} no puede ser sanado.`);
+        }
     }
-    
-} 
+}
+
+module.exports = Curandera;
