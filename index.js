c098943023fec8e38dd88c372905ac151fafac79
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-09-17 08:58:22 -0500

    feat: :sparkles: create menu
    create menu and functions
    
    By@CamilaFlorez

--- index.js
+++ index.js
@@ -5,67 +5,144 @@ const Aliado = require('./aliado');
 
 let personajes = [];
 
-function menu(){
+function menu() {
     inquirer.prompt([{
-        type:'list',
-        name:'option',
-        message:'Bienvenido a nuestro juego, selecciona la opcionas que deseas hacer:',
-        choices:[
-            {name:"1. Crear personaje",value:"1"},
-            {name:"2. Ver personajes",value:"2"},
-            {name:"3. Iniciar batalla", value:"3"},
-            {name:"4. Salir",value:"4"}
+        type: 'list',
+        name: 'option',
+        message: 'Bienvenido a nuestro juego, selecciona la opcionas que deseas hacer:',
+        choices: [
+            { name: "1. Crear personaje", value: "1" },
+            { name: "2. Ver personajes", value: "2" },
+            { name: "3. Iniciar batalla", value: "3" },
+            { name: "4. Salir", value: "4" }
         ]
     }])
-    .then((respuesta)=>{
-        switch(respuesta.option){
-            case "1":
-                crearPersonaje();
-                break;
-            case "2":
-                verPersonajes();
-                break;
-            case "3":
-                iniciarBatalla();
-                break;
-            case "4":
-                console.log("Gracias por jugar...");
-                break;
-        }
-    })
+        .then((respuesta) => {
+            switch (respuesta.option) {
+                case "1":
+                    crearPersonaje();
+                    break;
+                case "2":
+                    verPersonajes();
+                    break;
+                case "3":
+                    iniciarBatalla();
+                    break;
+                case "4":
+                    console.log("Gracias por jugar...");
+                    break;
+            }
+        })
 }
 
-function crearPersonaje(){
-    inquirer.prompt([{
-        type:'list',
-        name:'tipo',
-        message:'¿Qué tipo de personaje deseas crear?',
-        choices:['Maga','Arquera','Curandera','guerrera','Villano']
-    },
-    {
-        type:'input',
-        name:'nombre',
-        message:'Escribe el nombre del personaje',
-    },{
-        type:'number',
-        name:'nivel',
-        message:'Nivel inicial:',
-        default:1
-    },{
-        type:'number',
-        name:'poder',
-        message:'Poder inicial:',
-        default:20
-    },{
-        type:'number',
-        name:'ataque',
-        message:'Ataque inicial: ',
-        default:10
-    },{
-        type:'number',
-        name:'vida',
-        message:'Vida inicial:',
-        default:100
+function crearPersonaje() {
+
+    const personajeTipo = {
+        'Maga': ["Elsa"],
+        'Arquera': ["Merida"],
+        'Curandera': ["Rapunzel"],
+        'Guerrera': ["Mulan"],
+        'Villano': ["Maléfica", "Úrsula", "Jafar", "Madre Gothel"]
     }
-])
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'tipo',
+        message: '¿Qué tipo de personaje deseas crear?',
+        choices: ['Maga', 'Arquera', 'Curandera', 'Guerrera', 'Villano']
+
+    }]).then((respuesta) => {
+        const tipoSeleccionado = respuesta.tipo;
+        const disponibles = personajeTipo[tipoSeleccionado];
+
+        inquirer.prompt([{
+            type: 'list',
+            name: 'personajes',
+            message: 'Escoge tu personaje:',
+            choices: disponibles
+        }, {
+            type: 'number',
+            name: 'nivel',
+            message: 'Nivel inicial:',
+            default: 1
+        }, {
+            type: 'number',
+            name: 'poder',
+            message: 'Poder inicial:',
+            default: 20
+        }, {
+            type: 'number',
+            name: 'ataque',
+            message: 'Ataque inicial: ',
+            default: 10
+        }, {
+            type: 'number',
+            name: 'vida',
+            message: 'Vida inicial:',
+            default: 100
+        },{
+            type:''
+        }, {
+            type: 'list',
+            name: 'objetoMagico',
+            message: 'Elege un objeto mágico:',
+            choices: ['copo de nieve', 'espada ancestral', 'arco y flecha', 'flor de loto', 'bastón mágico', 'vara de la serpiente']
+        }]).then((personajeGuardado) => {
+            let habilidadEspecial;
+            switch (tipoSeleccionado) {
+                case "Maga":
+                    habilidadEspecial = "congelamiento";
+                    break;
+                case "Arquera":
+                    habilidadEspecial = "Punteria";
+                    break;
+                case "Curandera":
+                    habilidadEspecial = "Sanación";
+                    break;
+                case "Guerrera":
+                    habilidadEspecial = "Fuerza, Precisión y Punteria";
+                    break;
+                case "Villano":
+                    habilidadEspecial = "Magia oscura";
+                    break;
+                default:
+                    habilidadEspecial ="Desconocida";
+                    break;
+            }
+            personajes.push({
+                tipo: tipoSeleccionado,
+                nombre: personajeGuardado.personajes,
+                nivel: personajeGuardado.nivel,
+                poder: personajeGuardado.poder,
+                ataque: personajeGuardado.ataque,
+                vida: personajeGuardado.vida,
+                objetomagico: personajeGuardado.objetoMagico,
+                habilidadEspecial:habilidadEspecial
+            });
+            console.log("Personaje creado :)");
+            menu();
+        })
+
+    });
+};
+
+function verPersonajes(){
+    if(personajes.length === 0){
+        console.log("No hay personajes creados");
+    }else{
+        personajes.forEach((personaje,index)=>{
+        console.log('+++++++++++++++++++++++++++++++++');
+        console.log(`Personajes: ${index +1}:`);
+        console.log(`Tipo: ${personaje.tipo}`);
+        console.log(`Nombre: ${personaje.nombre}`);
+        console.log(`Nivel: ${personaje.nivel}`);
+        console.log(`Poder: ${personaje.poder}`);
+        console.log(`Ataque: ${personaje.ataque}`);
+        console.log(`Vida: ${personaje.vida}`);
+        console.log(`Habilidad especial:${personaje.habilidadEspecial}`);
+        console.log(`Objeto mágico: ${personaje.objetomagico}`);
+        console.log('++++++++++++++++++++++++++++++++++')
+    })
+    }
+    
 }
